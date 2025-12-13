6fdf67a925109ae8c619e30df426a31e49c1f6bc
Yifan Wu <tyivanwu@gmail.com>
2025-12-12 19:49:41 -0500

    chore: lint

--- 11/main.py
+++ 11/main.py
@@ -1,7 +1,7 @@
 import fileinput
+from collections import defaultdict, deque
 from functools import cache
 from pathlib import Path
-from collections import defaultdict, deque
 
 ROOT_DIR = Path(__file__).resolve().parents[0]
 INPUT_FILE = ROOT_DIR / "input.txt"
